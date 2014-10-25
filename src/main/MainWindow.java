84f2f2e63d58bbf00a4dd4372f3556d9ccd77242
Artúr Poór <gombocarti@gmail.com>
2014-10-25 23:37:36 +0200

    removed unused import

--- src/main/MainWindow.java
+++ src/main/MainWindow.java
@@ -2,7 +2,6 @@ package main;
 
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.LayoutManager2;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.LinkedList;
